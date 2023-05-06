1a9b5babac189fb158342fd676eb216b4609ff33
ariful2013 <ariful2013official@gmail.com>
2023-05-06 17:07:10 +0600

    New Item add Final

---
+++ puddle/item/forms.py
@@ -0,0 +1,27 @@
+from django import forms
+from .models import Item
+
+INPUT_CLASSES = 'w-full py-2 px-3 rounded border bg-gray-100 '
+
+
+class AddItemForm(forms.ModelForm):
+    class Meta:
+        model = Item
+        fields = ('category', 'name', 'description', 'price', 'image')
+        widgets = {
+            'category': forms.Select(attrs={
+                'class': INPUT_CLASSES + 'bg-gray-500 text-white'
+            }),
+            'name': forms.TextInput(attrs={
+                'class': INPUT_CLASSES
+            }),
+            'description': forms.Textarea(attrs={
+                'class': INPUT_CLASSES
+            }),
+            'price': forms.TextInput(attrs={
+                'class': INPUT_CLASSES
+            }),
+            'image': forms.FileInput(attrs={
+                'class': INPUT_CLASSES
+            }),
+        }
