8f290d0a8ffe1b4530c215872c2f3e4a86b3e69f
ariful2013 <ariful2013official@gmail.com>
2023-05-06 23:44:58 +0600

    Edit Item Final

--- puddle/item/urls.py
+++ puddle/item/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('additem/', views.addItem, name='additem'),
     path('<int:pk>/', views.detail, name='detail'),
     path('<int:pk>/delete/', views.deleteItem, name='deleteitem'),
+    path('<int:pk>/edit/', views.editItem, name='edititem'),
 ]
